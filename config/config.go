94cbd470be85714905a6e089f59ee8a87388e1f4
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-08-16 19:42:28 +0530

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+type Config struct {
+    // Configuration options
+}
+
+func LoadConfig() Config {
+    // Load configuration from file or environment variables
+    // Return a populated Config struct
+}
