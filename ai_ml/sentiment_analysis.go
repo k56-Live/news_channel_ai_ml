7db6469daf43c3ee5d320e9c83984288b1fc0fa1
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-08-16 19:41:01 +0530

    Create sentiment_analysis.go

---
+++ ai_ml/sentiment_analysis.go
@@ -0,0 +1,10 @@
+package ai_ml
+
+import (
+    "news_channel_ai_ml/internal/news"
+)
+
+func AnalyzeSentiment(newsItem news.News) string {
+    // Sentiment analysis logic here
+    // Return sentiment label (positive, negative, neutral)
+}
