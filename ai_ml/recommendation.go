e7e19da55aa24ae11cb3bd49b30096fd5f546eae
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-08-16 19:41:22 +0530

    Create recommendation.go

---
+++ ai_ml/recommendation.go
@@ -0,0 +1,10 @@
+package ai_ml
+
+import (
+    "news_channel_ai_ml/internal/news"
+)
+
+func GenerateRecommendations(userProfile UserProfile) []news.News {
+    // Recommendation generation logic here
+    // Return recommended news items
+}
